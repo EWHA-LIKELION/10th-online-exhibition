bcef305c6a2bc9b19389dba5779ddcef3c867b92
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-01-14 20:15:42 +0900

    🧪 : 360회전 애니메이션 test

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '../styles/globals.css'
+import "../styles/test.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }

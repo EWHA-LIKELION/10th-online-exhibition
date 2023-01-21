6e42fcbe4e7de1e0749734e8b867a6d9e3ab5d64
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-01-21 20:15:46 +0900

    🚀 : 네브바 Link 추가

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import Image from "next/image";
-
+import Link from "next/link";
 //이미지
 //import navbar from "../assets/main/navbar3.png";
 import navbar from "../assets/main/navbar2.svg";
@@ -9,6 +9,16 @@ const Navbar = () => {
   return (
     <NavDiv>
       <Image src={navbar} fill />
+
+      <Link href="/likelion">
+        <div className="second"></div>
+      </Link>
+      <Link href="/lions">
+        <div className="third"> </div>
+      </Link>
+      <Link href="/makers">
+        <div className="fourth"> </div>
+      </Link>
     </NavDiv>
   );
 };
@@ -28,5 +38,37 @@ const NavDiv = styled.div`
     @media screen and (min-width: 320px) {
       transform: translateX(calc((100% - 320px) / 2 * (-1)));
     }
+  }
+  div {
+    position: absolute;
+    width: 10px;
+    height: 36px;
+  }
+  /* .first {
+    border: 1px solid red;
+    left: 15px;
+    width: 27px;
+    height: 30px;
+  } */
+
+  .second {
+    border: 1px solid red;
+    left: 15px;
+    width: 120px;
+    height: 30px;
+  }
+
+  .third {
+    border: 1px solid red;
+    left: 155px;
+    width: 71px;
+    height: 30px;
+  }
+
+  .fourth {
+    border: 1px solid red;
+    left: 260px;
+    width: 45px;
+    height: 30px;
   }
 `;

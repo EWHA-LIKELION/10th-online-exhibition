9df576d14f2227e7dcf9103e38a563e3147af584
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-01-21 20:01:57 +0900

    🎨 : Navbar 위치 조정

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,34 @@
+import styled from "styled-components";
+import Image from "next/image";
+
+//이미지
+//import navbar from "../assets/main/navbar3.png";
+import navbar from "../assets/main/navbar2.svg";
+
 const Navbar = () => {
-  return <p>네브바</p>;
+  return (
+    <NavDiv>
+      <Image src={navbar} fill />
+    </NavDiv>
+  );
 };
 
 export default Navbar;
+
+// 이미지 실제 가로 : 320
+
+const NavDiv = styled.div`
+  border: 1px solid blue;
+
+  position: relative;
+
+  margin-top: 33px;
+  width: 100%;
+  height: 46px;
+
+  img {
+    @media screen and (min-width: 320px) {
+      transform: translateX(calc((100% - 320px) / 2 * (-1)));
+    }
+  }
+`;

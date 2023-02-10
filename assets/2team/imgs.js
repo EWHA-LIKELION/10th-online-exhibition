2de5aea1ec72384708334720c0ff8c994f78fbb2
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-02-10 18:12:52 +0900

    🔨 : detail 페이지 리팩토링

---
+++ assets/2team/imgs.js
@@ -0,0 +1,16 @@
+import img1 from "./img1.png";
+import img2 from "./img2.png";
+
+export default [
+  img1,
+  img2,
+  img2,
+  img2,
+  img2,
+  img2,
+  img2,
+  img2,
+  img2,
+  img2,
+  img2,
+];
--- pages/detail/[id]/index.js
+++ pages/detail/[id]/index.js
@@ -1,9 +1,9 @@
+import { useState, useEffect } from "react";
 // 졸프 상세 페이지
-import { useRouter } from "next/router";
-import { useState } from "react";
 import Image from "next/image";
 import styled from "styled-components";
 import Link from "next/link";
+
 //components
 import Footer from "../../../components/Footer";
 //image
@@ -12,17 +12,42 @@ import back from "../../../assets/topbar/back.svg";
 import namingtitle1 from "../../../assets/detail/namingtitle1.svg";
 import namingtitle2 from "../../../assets/detail/namingtitle2.svg";
 
+import thumbnail2 from "../../../assets/2team/thumbnail.png";
 import introduceimg from "../../../assets/detail/introduceimg.svg";
 import rnrimg from "../../../assets/detail/rnrimg.svg";
 import techstackimg from "../../../assets/detail/techstackimg.svg";
-import pdtech1 from "../../../assets/detail/pdtech1.svg";
-import pdtech2 from "../../../assets/detail/pdtech2.svg";
 import btnicon from "../../../assets/detail/btnicon.svg";
 
+// tech img
+import figma from "../../../assets/detail/figma.svg";
+import ai from "../../../assets/detail/ai.svg";
+import react from "../../../assets/detail/react.svg";
+import npm from "../../../assets/detail/npm.svg";
+import github from "../../../assets/detail/github.svg";
+import django from "../../../assets/detail/django.svg";
+import aws from "../../../assets/detail/aws.svg";
+import git from "../../../assets/detail/git.svg";
+
+// ui 이미지
+import team1 from "../../../assets/2team/imgs";
+
+import img1 from "../../../assets/2team/img1.png";
+
 const Detail = (props) => {
+  const [w, setW] = useState(0);
+
+  useEffect(() => {
+    setW((window.innerWidth - 30).toString() + "px");
+    console.log(w);
+  });
+
+  const Imgs = [team1, team1];
+
   const { id } = props;
+  const Data = Introducedata[id - 1];
   const Task = RnRdata[id - 1].task;
-  const Tech = Techdata[id - 1].tech;
+  const Tech = Techdata[id - 1];
+
   console.log(Tech);
 
   return (
@@ -31,12 +56,8 @@ const Detail = (props) => {
         style={{
           backgroundImage: `url(${backimg.src})`,
           backgroundSize: "cover",
-          width: "100%",
-          height: "100%",
         }}
       >
-        {/* <Image src={backimg.src} className="background" fill /> */}
-
         <TopBar>
           <Link href="/">
             <Image src={back.src} width={12} height={24} alt="back" />
@@ -49,12 +70,20 @@ const Detail = (props) => {
             alt="titleimg"
           />
         </TopBar>
-        <Slider />
+
+        <Thumbnail>
+          <Image
+            src={Data.thumbnail}
+            width={390}
+            height={243}
+            className="thumbnail"
+          />
+        </Thumbnail>
+
         <div
           style={{
             display: "flex",
             "flex-direction": "column",
-            // marginBottom: "30px",
           }}
         >
           <Image
@@ -64,10 +93,18 @@ const Detail = (props) => {
             alt="introduceimg"
             className="index"
           />
+
           <Introoduce>
-            <p>{Introducedata[id - 1].oneline}</p>
-            <span>{Introducedata[id - 1].detail}</span>
+            <p className="title">{Introducedata[id - 1].oneline}</p>
+            <p className="des"> {Introducedata[id - 1].detail}</p>
           </Introoduce>
+
+          <Slider width={w}>
+            {Imgs[id - 1].map((i) => {
+              return <Card src={i.src} key={i.src} />;
+            })}
+          </Slider>
+
           <Image
             src={rnrimg.src}
             width={358}
@@ -91,6 +128,7 @@ const Detail = (props) => {
               })}
             </TaskBox>
           </RnR>
+
           <RnR>
             <span className="part-name">프론트엔드</span>
             <TaskBox>
@@ -129,76 +167,68 @@ const Detail = (props) => {
             alt="techstackimg"
             className="index"
           />
+
           <TechBox>
             <p className="part-name">기획 디자인</p>
 
-            {Tech.map((tech) => {
-              if (tech.part == 1) {
-                return (
-                  <Image
-                    key={tech.alt}
-                    src={tech.src.src}
-                    width={50}
-                    height={50}
-                    className="techicon"
-                    alt="techicon"
-                  />
-                );
-              }
+            {Tech.tech1.map((tech) => {
+              return (
+                <Image
+                  key={tech.alt}
+                  src={tech.src.src}
+                  width={50}
+                  height={50}
+                  className="techicon"
+                  alt="techicon"
+                />
+              );
             })}
           </TechBox>
           <TechBox>
             <p className="part-name">프론트엔드</p>
-            {Tech.map((tech) => {
-              if (tech.part == 2) {
-                return (
-                  <Image
-                    key={tech.alt}
-                    src={tech.src.src}
-                    width={50}
-                    height={50}
-                    className="techicon"
-                    alt="techicon"
-                  />
-                );
-              }
+            {Tech.tech2.map((tech) => {
+              return (
+                <Image
+                  key={tech.alt}
+                  src={tech.src.src}
+                  width={50}
+                  height={50}
+                  className="techicon"
+                  alt="techicon"
+                />
+              );
             })}
           </TechBox>
           <TechBox>
             <p className="part-name">백엔드</p>
-            {Tech.map((tech) => {
-              if (tech.part == 3) {
-                return (
-                  <Image
-                    key={tech.alt}
-                    src={tech.src.src}
-                    width={50}
-                    height={50}
-                    className="techicon"
-                    alt="techicon"
-                  />
-                );
-              }
+            {Tech.tech3.map((tech) => {
+              return (
+                <Image
+                  key={tech.alt}
+                  src={tech.src.src}
+                  width={50}
+                  height={50}
+                  className="techicon"
+                  alt="techicon"
+                />
+              );
             })}
           </TechBox>
         </div>
 
-        <div
-          style={{ width: "10%", display: "flex", justifyContent: "center" }}
-        >
-          <Link href={Introducedata[id - 1].url}>
-            <VisitBtn>
-              <p>웹사이트 방문하기</p>
-              <Image
-                src={btnicon.src}
-                width={20}
-                height={20}
-                className="btn"
-                alt="btn"
-              />
-            </VisitBtn>
-          </Link>
-        </div>
+        <Link href={Introducedata[id - 1].url}>
+          <VisitBtn>
+            <p>웹사이트 방문하기</p>
+            <Image
+              src={btnicon.src}
+              width={20}
+              height={20}
+              className="btn"
+              alt="btn"
+            />
+          </VisitBtn>
+        </Link>
+
         <Footer />
       </Container>
     </Wrapper>
@@ -224,7 +254,46 @@ export const getStaticPaths = async () => {
   };
 };
 
+const Slider = styled.div`
+  width: ${(props) => props.width};
+  height: 286px;
+
+  margin: 30px 16px 0 16px;
+  display: flex;
+
+  overflow-x: scroll;
+
+  padding-bottom: 12px;
+
+  &::-webkit-scrollbar {
+    height: 6px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #d0e3d5; /* 스크롤바의 색상 */
+    border-radius: 100px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #ffffff; /*스크롤바 뒷 배경 색상*/
+  }
+
+  @media screen and (min-width: 440px) {
+    width: 390px;
+    height: 286px;
+  }
+`;
+
+const Card = styled.img`
+  margin-left: 8px;
+  width: 140px;
+  height: 268px;
+`;
+
 const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+
   position: relative;
   @media screen and (min-width: 440px) {
     width: 390px;
@@ -232,17 +301,15 @@ const Wrapper = styled.div`
   }
 `;
 const Container = styled.div`
-  @media screen and (min-width: 440px) {
-    width: 390px;
-    height: auto;
-  }
+  width: 100%;
+  height: 100%;
 
   padding-bottom: 120px;
   position: relative;
-
+  /* 
   &::-webkit-scrollbar {
     display: none;
-  }
+  } */
 
   a {
     width: fit-content;
@@ -270,10 +337,86 @@ const Container = styled.div`
     padding-bottom: 15px;
   }
 
-  .part-name {
-    width: 100px;
-    white-space: nowrap;
+  @media screen and (min-width: 440px) {
+    width: 390px;
+    height: auto;
+  }
+`;
 
+const TopBar = styled.div`
+  position: relative;
+  top: 0;
+  width: 100%;
+  height: 100px;
+  margin-top: 30px;
+
+  background-color: transparent;
+  display: flex;
+  a {
+    padding: 38px 20px;
+    position: absolute;
+  }
+
+  .titleimg {
+    width: 100%;
+  }
+`;
+
+const Thumbnail = styled.div`
+  .thumbnail {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Introoduce = styled.div`
+  word-break: break-all;
+  box-sizing: border-box;
+  padding: 0 20px 0 20px;
+
+  width: 100%;
+  height: auto;
+
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  align-items: center;
+
+  .title {
+    font-family: "Yde street";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 18px;
+    margin-bottom: 10px;
+  }
+
+  .des {
+    width: 100%;
+
+    font-family: "Pretendard";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: -0.442px;
+  }
+
+  @media screen and (min-width: 440px) {
+    padding: 0 30px 0 30px;
+  }
+`;
+
+const RnR = styled.div`
+  width: 100%;
+  padding-left: 22px;
+
+  display: flex;
+  position: relative;
+
+  .part-name {
+    width: 69px;
+    white-space: nowrap;
     font-family: "Pretendard Variable";
     font-style: normal;
     font-weight: 600;
@@ -283,7 +426,6 @@ const Container = styled.div`
 
   .mem-name {
     margin-bottom: 10px;
-
     font-family: "Pretendard";
     font-style: normal;
     font-weight: 400;
@@ -298,74 +440,16 @@ const Container = styled.div`
     font-size: 14px;
     line-height: 17px;
     margin-bottom: 20px;
-
     color: #5c5c5c;
   }
-`;
 
-const TopBar = styled.div`
-  position: relative;
-  top: 0;
-  width: 100%;
-  height: 100px;
-  padding: 20px;
-  background-color: transparent;
-  display: flex;
-  a {
-    padding: 30px 20px;
-    position: absolute;
+  @media screen and (min-width: 440px) {
+    padding-left: 30px;
   }
-
-  .titleimg {
-    width: 100%;
-  }
-`;
-
-const Slider = styled.div`
-  min-width: 436px;
-  min-height: 300px;
-  background-color: grey;
-`;
-
-const Introoduce = styled.div`
-  width: 100%;
-  height: 100%;
-
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  align-items: center;
-
-  p {
-    font-family: "Yde street";
-    font-style: normal;
-    font-weight: 700;
-    font-size: 14px;
-    line-height: 18px;
-    margin-bottom: 10px;
-  }
-
-  span {
-    width: 90%;
-    font-family: "Pretendard";
-    font-style: normal;
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 20px;
-    letter-spacing: -0.442px;
-  }
-`;
-
-const RnR = styled.div`
-  width: 90%;
-  height: 100%;
-  margin: 0 auto;
-
-  display: flex;
-  position: relative;
 `;
 
 const TaskBox = styled.div`
+  width: 70%;
   display: flex;
   flex-direction: column;
 `;
@@ -374,19 +458,34 @@ const TechBox = styled.div`
   width: 90%;
   margin: 5px auto 10px auto;
 
+  .part-name {
+    width: 69px;
+    white-space: nowrap;
+    font-family: "Pretendard Variable";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 17px;
+  }
   .techicon {
     margin-top: 11px;
     margin-right: 14px;
+  }
+
+  @media screen and (min-width: 440px) {
+    margin: 5px 10px 10px auto;
   }
 `;
 
 const VisitBtn = styled.div`
   z-index: 1000000;
   position: fixed;
-  width: 328px;
+
+  width: 80%;
   height: 50px;
+
   bottom: 25px;
-  left: calc((100% - 328px) / 2);
+  left: calc((20%) / 2);
 
   background: #75a483;
   border: 0;
@@ -419,6 +518,7 @@ const Introducedata = [
     name: "이름하여 이름하다",
     url: "https://www.instagram.com/likelion_ewha/",
     titleimg: namingtitle1,
+    thumbnail: thumbnail2,
     oneline: "나만의 사전을 친구들과 함께 채워가요",
     detail: `목숨이 위하여, 눈에 봄바람이다. 인간의 대고, 불어 이상의 목숨이 이 있는가? 속잎나고, 기관과 설산에서 봄바람이다. 청춘의 것이 속에서 눈에 군영과 보이는 있으랴? 뭇 끓는 지혜는 사는가 싹이 풍부하게 않는 그들에게 하여도 것이다. 
     설레는 너의 내는 트고, 새 긴지라 아름다우냐? 우리 대중을 위하여서, 그들의 가진 같으며, 피부가 작고 것이다. 군영과 것은 우리의 이상의 불어 영락과 그리하였는가?`,
@@ -426,11 +526,13 @@ const Introducedata = [
   {
     id: 2,
     name: "응원이 부적해",
-    url: "https://www.instagram.com/likelion_ewha/",
+    url: "https://cheer-charm.swygbro.com/",
     titleimg: namingtitle2,
-    oneline: "나만의 사전을 친구들과 함께 채워가요",
-    detail: `목숨이 위하여, 눈에 봄바람이다. 인간의 대고, 불어 이상의 목숨이 이 있는가? 속잎나고, 기관과 설산에서 봄바람이다. 청춘의 것이 속에서 눈에 군영과 보이는 있으랴? 뭇 끓는 지혜는 사는가 싹이 풍부하게 않는 그들에게 하여도 것이다. 
-        설레는 너의 내는 트고, 새 긴지라 아름다우냐? 우리 대중을 위하여서, 그들의 가진 같으며, 피부가 작고 것이다. 군영과 것은 우리의 이상의 불어 영락과 그리하였는가?`,
+    thumbnail: thumbnail2,
+
+    oneline: "응원으로 완성되는 나만의 커스텀 부적 만들기",
+    detail:
+      "내가 지은 이름과 내가 고른 디자인의 커스텀 부적을 생성하고 링크를 공유할 수 있어요. 응원이 모두 모이기 전까지는 부적과 응원 내용을 볼 수 없지만, 목표한 응원 개수를 달성하게 되면 완성된 부적 이미지를 저장할 수 있어요.  친구의 부적에 익명으로 닉네임과 내용을 남겨 응원할 수 있고, 내용은 부적을 생성한 사람만 열람할 수 있는 비공개 응원이에요. 마이페이지에서 언제든 내 부적들을 다시 보고 삭제해 보세요.",
   },
 ];
 
@@ -488,37 +590,37 @@ const RnRdata = [
       {
         part: 1,
         name: "박성연",
-        task: `맡은 일, 맡은 일, 맡은일, 맡은일, 맡은 일, 맡은`,
+        task: "응원이 부적해 서비스 기획,  랜딩 페이지 / 마이 페이지 / 응원 작성 페이지 / 부적 이미지 디자인 ",
       },
       {
         part: 1,
         name: "김다은",
-        task: `맡은 일, 맡은 일 맡은 일, 맡은 일, 맡은 일`,
+        task: "부적 링크, 결과 페이지 / 로그인,회원가입 페이지 디자인",
       },
       {
         part: 2,
         name: "정연주",
-        task: `맡은 일, 맡은 일 일, 맡은 일, 맡은 일, 맡은 일`,
+        task: "로그인/회원가입, 랜딩 페이지, 응원 전송 페이지, 부적 결과 페이지(부적)",
       },
       {
         part: 2,
         name: "이서진",
-        task: `맡은 일, 맡은 일, 맡 맡은 일, 맡은 일, 맡은 일`,
+        task: "부적 생성 페이지, 마이페이지, 부적 공유 페이지, 부적 결과 페이지(응원)",
       },
       {
         part: 3,
         name: "조현영",
-        task: `맡은 일, 맡은 일, 일, 맡은 일, 맡은 일, 맡은 일`,
+        task: "응원하기, 이미지 업로드, ERD 설계, API 명세서 작성, 배포",
       },
       {
         part: 3,
         name: "최유미",
-        task: `맡은 일맡은 일, 맡은 일`,
+        task: "로그인/로그아웃, 회원가입, ERD 설계, API 명세서 작성",
       },
       {
         part: 3,
         name: "이나경",
-        task: `맡은 일, 맡은 , 맡은 일, 맡은 일, 맡은 일`,
+        task: "부적 생성하기, 부적함 보기, ERD 설계, API 명세서 작성",
       },
     ],
   },
@@ -528,25 +630,25 @@ const Techdata = [
   {
     id: 1,
     name: "이름하여 이름하다",
-    tech: [
-      { src: pdtech1, alt: "figma", part: 1 },
-      { src: pdtech2, alt: "Adobe Illustrator", part: 1 },
-      { src: pdtech1, alt: "figma", part: 2 },
-      { src: pdtech2, alt: "Adobe Illustrator", part: 2 },
-      { src: pdtech1, alt: "figma", part: 3 },
-      { src: pdtech2, alt: "Adobe Illustrator", part: 3 },
-    ],
+    tech: [{ src: figma, alt: "figma", part: 1 }],
   },
   {
     id: 2,
     name: "응원이 부적해",
-    tech: [
-      { src: pdtech1, alt: "figma", part: 1 },
-      { src: pdtech2, alt: "Adobe Illustrator", part: 1 },
-      { src: pdtech1, alt: "figma", part: 2 },
-      { src: pdtech2, alt: "Adobe Illustrator", part: 2 },
-      { src: pdtech1, alt: "figma", part: 3 },
-      { src: pdtech2, alt: "Adobe Illustrator", part: 3 },
+    tech1: [
+      { src: figma, alt: "figma" },
+      { src: ai, alt: "ai" },
+    ],
+
+    tech2: [
+      { src: react, alt: "react", part: 2 },
+      { src: npm, alt: "npm", part: 2 },
+      { src: github, alt: "github", part: 2 },
+    ],
+    tech3: [
+      { src: django, alt: "django", part: 3 },
+      { src: aws, alt: "aws", part: 3 },
+      { src: git, alt: "git", part: 3 },
     ],
   },
 ];
